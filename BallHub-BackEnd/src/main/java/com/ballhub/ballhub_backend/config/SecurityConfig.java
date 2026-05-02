892aabb43ad65b5a9dc2f826d925c400f9941d75
Mochi The Dev 🤓 <nguyendangphuc2611@gmail.com>
2026-05-02 14:25:36 +0700

    Add allowed origins for CORS configuration

--- BallHub-BackEnd/src/main/java/com/ballhub/ballhub_backend/config/SecurityConfig.java
+++ BallHub-BackEnd/src/main/java/com/ballhub/ballhub_backend/config/SecurityConfig.java
@@ -99,7 +99,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "https://ballhub-front-end.vercel.app", "https://ballhub-front-cuaokppf5-dangphuc2611s-projects.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
